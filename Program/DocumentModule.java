32a3c788c72b0077157dd199631330af94abd9e2
KattrinSue <sycheva.ev2001@gmail.com>
2023-03-28 14:58:38 +0300

    update document module class description

--- Program/DocumentModule.java
+++ Program/DocumentModule.java
@@ -1,5 +1,17 @@
+import java.nio.file.Files;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
 // А) модуль работы с документами
 public class DocumentModule {
+    List<String> data;
+    JFrame frame;
+    String path;
+
+    public DocumentModule(JFrame frame) {
+        this.frame = frame;
+    }
+    
     public void openDocument(String filename) {
         // Реализовать открытие документа
     }
