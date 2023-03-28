8ab5ed18a73d72ce247b09cefa1c02bb28f39d43
KattrinSue <sycheva.ev2001@gmail.com>
2023-03-28 14:56:39 +0300

    create document module class description

---
+++ Program/DocumentModule.java
@@ -0,0 +1,14 @@
+// А) модуль работы с документами
+public class DocumentModule {
+    public void openDocument(String filename) {
+        // Реализовать открытие документа
+    }
+    
+    public void createDocument(String filename) {
+        // Реализовать создание документа
+    }
+    
+    public void saveDocument(String filename) {
+        // Реализовать сохранение документа
+    }
+}
