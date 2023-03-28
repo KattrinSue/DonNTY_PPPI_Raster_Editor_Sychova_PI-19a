630fb68721869d890989f70571ff60e5684bed98
KattrinSue <sycheva.ev2001@gmail.com>
2023-03-28 22:01:11 +0300

    updated layers module class description

---
+++ Program/LayersModule.java
@@ -0,0 +1,22 @@
+// Г) модуль слоев
+public class LayersModule {
+    public void createLayer() {
+        // Реализовать создание слоя
+    }
+    
+    public void duplicateLayer() {
+        // Реализовать дублирование слоя
+    }
+    
+    public void deleteLayer() {
+        // Реализовать удаление слоя
+    }
+    
+    public void groupLayers(List<Layer> layers) {
+        // Реализовать группирование слоев
+    }
+    
+    public void ungroupLayers(List<Layer> layers) {
+        // Реализовать разгруппирование слоев
+    }
+}
