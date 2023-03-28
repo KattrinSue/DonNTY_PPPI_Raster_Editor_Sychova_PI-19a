f74fb497baa9fefa28501a99b85f45d46eb820c8
KattrinSue <sycheva.ev2001@gmail.com>
2023-03-28 22:02:12 +0300

    updated text module class description

---
+++ Program/TextModule.java
@@ -0,0 +1,10 @@
+// Д) модуль текста
+public class TextModule {
+    public void openSymbolsPanel() {
+        // Реализовать открытие панели символов
+    }
+    
+    public void loadFontPackage(String filename) {
+        // Реализовать загрузку пакета шрифтов
+    }
+}
